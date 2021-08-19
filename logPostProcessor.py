4fd13070e8a49eeba1b674fb59becb56525062d8
mpucci92 <42786192+mpucci92@users.noreply.github.com>
2021-08-18 21:24:53 -0400

    Add files via upload

---
+++ logPostProcessor.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta,time,date
+import os
+
+currentDate = date.today()
+currentDate = currentDate.strftime("%Y-%m-%dT%H:%M:%S")
+currentDate = currentDate.split('T')[0]
+
+filename = 'logPostProcessor.py'
+path = os.path.abspath(filename)
+directory = os.path.dirname(path)
+saveLocationLogs = directory + "\\Logs\\"
+
+def drop_consecutive_duplicates(a):
+    ar = a.values
+    return ar[np.concatenate(([True],ar[:-1]!= ar[1:]))]
+
+def logPostProcess():
+    logText = []
+    file = saveLocationLogs + f'{currentDate}' + '.log'
+    Logfile = saveLocationLogs + f'{currentDate}' + '.txt'
+
+    with open(file) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        logText.append(line.split(' - ')[1])
+
+    df = pd.DataFrame()
+    df['logtext'] = logText
+    a = df.logtext
+
+    dfNew = pd.DataFrame()
+    dfNew['finaltext'] = drop_consecutive_duplicates(a)
+    dfNew.to_csv(Logfile, index=False, header=False)
